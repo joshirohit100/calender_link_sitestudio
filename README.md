d25638a05da85f7017df6c5f597bcb548bee1388
joshirohit100 <joshi.rohit100@gmail.com>
2025-02-07 15:14:48 +0530

    first commit

---
+++ README.md
@@ -0,0 +1,28 @@
+This drupal module is dependent on <link>https://www.drupal.org/project/calendar_link</link>
+
+This module provides [Acquia Site Studio](https://sitestudiodocs.acquia.com/) integration
+of [Calendar Link](https://www.drupal.org/project/calendar_link) module.
+
+This provides an element which can be used in component to create
+links for the calendar
+
+Currently supported calendar
+<ul>
+  <li>Apple</li>
+  <li>Google</li>
+  <li>Yahoo</li>
+  <li>Outlook</li>
+  <li>Office</li>
+</ul>
+
+## Steps for usage
+<ul>
+  <li>Visit the component page</li>
+  <li>Select "Calender Link SiteStudio" from element</li>
+  <li>Select the calendars you want</li>
+  <li>Fill the other details</li>
+</ul>
+
+
+
+
---
+++ src/Plugin/CustomElement/CalenderLinkSiteStudio.php
@@ -0,0 +1,99 @@
+<?php
+
+namespace Drupal\calender_link_sitestudio\Plugin\CustomElement;
+
+use Drupal\cohesion_elements\CustomElementPluginBase;
+use Drupal\Core\StringTranslation\StringTranslationTrait;
+
+/**
+ * Site Studio element for "Calendar Link".
+ *
+ * @CustomElement(
+ *   id = "calender_link_sitestudio",
+ *   label = @Translation("Calender Link SiteStudio")
+ * )
+ */
+class CalenderLinkSiteStudio extends CustomElementPluginBase {
+
+  use StringTranslationTrait;
+
+  /**
+   * This is the number of calendar type support.
+   */
+  protected const int TOTAL_CALENDER_ITEMS = 5;
+
+  /**
+   * {@inheritdoc}
+   */
+  public function getFields(): array {
+    $total_calender_items = [];
+    for ($i = 0; $i < self::TOTAL_CALENDER_ITEMS; $i++) {
+      $total_calender_items['calender_item_' . $i] = [
+        'htmlClass' => 'ssa-grid-col-3',
+        'type' => 'select',
+        'title' => $this->t('Select Calenders'),
+        'nullOption' => TRUE,
+        'options' => [
+          'ics' => $this->t('Apple'),
+          'google' => $this->t('Google'),
+          'webOutlook' => $this->t('Outlook'),
+          'webOffice' => $this->t('Office'),
+          'yahoo' => $this->t('Yahoo'),
+        ],
+      ];
+    }
+
+    return $total_calender_items + [
+      'title' => [
+        'htmlClass' => 'ssa-grid-col-1',
+        'title' => $this->t('Calender title'),
+        'type' => 'textfield',
+        'placeholder' => $this->t('Title to be added in calender'),
+        'required' => TRUE,
+        'validationMessage' => $this->t('This field is required.'),
+      ],
+      'start_date' => [
+        'title' => $this->t('Calender start date'),
+        'type' => 'textfield',
+        'required' => TRUE,
+        'placeholder' => $this->t('Start date'),
+      ],
+      'end_date' => [
+        'title' => $this->t('Calender end date'),
+        'type' => 'textfield',
+        'required' => TRUE,
+        'placeholder' => $this->t('End date'),
+      ],
+      'full_day' => [
+        'type' => 'checkbox',
+        'title' => $this->t('Full day'),
+        'defaultValue' => FALSE,
+      ],
+      'description' => [
+        'title' => $this->t('Calender description'),
+        'type' => 'textarea',
+        'placeholder' => $this->t('Description to be added in calender'),
+      ],
+      'location' => [
+        'title' => $this->t('Calender Address'),
+        'type' => 'textarea',
+        'placeholder' => $this->t('Description to be added in calender'),
+      ],
+    ];
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function render($element_settings, $element_markup, $element_class): array {
+    return [
+      '#theme' => 'calender_link_sitestudio_element',
+      '#template' => 'calender-link-sitestudio-element',
+      '#elementSettings' => $element_settings,
+      '#elementMarkup' => $element_markup,
+      '#elementClass' => $element_class,
+      '#totalCalenderItems' => self::TOTAL_CALENDER_ITEMS,
+    ];
+  }
+
+}
